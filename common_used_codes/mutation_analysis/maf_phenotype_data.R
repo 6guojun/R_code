55f82c38f352c93e8e9e83f823b474fdf43970eb
stead99 <shangjunv@163.com>
2018-10-02 11:21:35 +0800

    mutation_add

--- common_used_codes/mutation_analysis/maf_phenotype_data.R
+++ common_used_codes/mutation_analysis/maf_phenotype_data.R
@@ -19,9 +19,20 @@ ContMafCli <- function(mut_mat, exp_phe_mat, phe_nam){
   return(mut_mat_cli_phe_d)
 }
 
-#mut_cli_phe <- ContMafCli(mut_sam, rt_mvm_sur_all_min)
-#maf_val = read.maf(maf = "/Users/stead/Desktop/PD-L1_and_TMI_type/UCSC_GDC_data/KIRP/somatic_mut/TCGA.KIRP.mutect.1ab98b62-5863-4440-84f9-3c15d476d523.DR-10.0.somatic.maf", 
-#                   clinicalData = mut_cli_phe)
+Addphe2Maf <- function(mut_mat, mat_phe){
+  mut_mat_mat <- matrix(unlist(strsplit(as.character(mut_mat$Tumor_Sample_Barcode), '-')), ncol = 7, byrow = TRUE)
+  mut_mat_cli <- data.frame(cbind(paste(mut_mat_mat[, 1], mut_mat_mat[, 2], mut_mat_mat[, 3], sep = "-"), as.character(mut_mat$Tumor_Sample_Barcode)), stringsAsFactors = FALSE)
+  
+  colnames(mut_mat_cli)[1] <- 'Tumor_Sample_Barcode'
+  colnames(mat_phe)[1] <- 'Tumor_Sample_Barcode'
+  
+  mut_mat_phe <- merge(mut_mat_cli, mat_phe, by = 'Tumor_Sample_Barcode')
+  mut_mat_phe <- mut_mat_phe[, -1]
+  colnames(mut_mat_phe)[1] <- 'Tumor_Sample_Barcode'
+  
+  return(mut_mat_phe)
+}
+
 
 CountMutPhe <- function(maf_val, maf_phe, clinicalFeatures = clinicalFeatures, gene_list, ggcolor = ggcolor, theme_mut){
   MutPheMat <- data.frame(left_join(left_join(left_join(maf_val@variants.per.sample, maf_val@variant.type.summary, by = 'Tumor_Sample_Barcode'), 
@@ -59,8 +70,105 @@ CountMutPhe <- function(maf_val, maf_phe, clinicalFeatures = clinicalFeatures, g
   }
 }
 
-#gene_list = c('SETD2', 'PBRM1')
-#CountMutPhe(maf_val, mut_cli_phe, clinicalFeatures = 'risk_score', gene_list)
+MafToolSum <- function(maf_val, phe_type = c('subtype', 'all'), sub1 = NULL, sub2 = NULL, clinicalFeatures = clinicalFeatures, 
+                       gene_list = gene_list, gene_sur = gene_sur, ggcolor = ggcolor, annotationColor = annotationColor, 
+                       col_sur = c("maroon", "royalblue")){
 
+  PermafSum <- function(maf_per, P_nam = P_nam){
+    pdf(paste('mafSummary_', P_nam, ".pdf", sep = ''))
+    plotmafSummary(maf = maf_per, rmOutlier = TRUE, addStat = 'median', dashboard = TRUE, titvRaw = FALSE)
+    dev.off()
+  }
+  
+  Peroncoplot <- function(maf_per, P_nam = P_nam, gene_list = gene_list, clinicalFeatures = clinicalFeatures, annotationColor = annotationColor){
+    pdf(paste("oncoplot_", P_nam, ".pdf", sep = ""))
+    oncoplot(maf = maf_per, top = 20, fontSize = 12, clinicalFeatures = clinicalFeatures, genes = gene_list, 
+             annotationColor = annotationColor)
+    dev.off()
+  }
+  
+  Peroncostrip <- function(maf_per, P_nam = P_nam, gene_list = gene_list, clinicalFeatures = clinicalFeatures, annotationColor = annotationColor){
+    pdf(paste("oncostrip_", P_nam, ".pdf", sep = ""), 10, 6)
+    oncostrip(maf = maf_per, clinicalFeatures = clinicalFeatures, genes = gene_list, annotationColor = annotationColor)
+    dev.off()
+    }
+  
+  PerTiTv <- function(maf_per, P_nam = P_nam){
+      maf_titv = titv(maf = maf_per, plot = FALSE, useSyn = TRUE)
+      #plot titv summary
+      pdf(paste("TiTv_", P_nam, ".pdf", sep = ""))
+      plotTiTv(res = maf_titv)
+      dev.off()
+  }
+  
+  PerVaf <- function(maf_per, P_nam = P_nam, gene_list = gene_list){
+    pdf(paste("Vaf_", P_nam, ".pdf", sep = ""))
+    plotVaf(maf = maf_per,  genes = gene_list)
+    dev.off()
+  }
+  
+  PerInteraction <- function(maf_per, P_nam = P_nam, gene_list = gene_list){
+    pdf(paste("Interaction_", P_nam, ".pdf", sep = ""))
+    somaticInteractions(maf = maf_per, top = 25, pvalue = c(0.05, 0.1),  genes = gene_list)
+    dev.off()
+  }
+  
+  PerSur <- function(gene_sur = gene_sur, maf_per, P_nam = P_nam, col = col_sur){
+    pdf(paste(gene_sur, P_nam, "sur.pdf", sep = "_"))
+    Sur_F <- mafSurvival(genes = gene_sur, maf = maf_per, time = 'OS_Time', Status = 'OS_Status', isTCGA = FALSE)
+    print(Sur_F)
+    dev.off()
+  }
+  
+  PerForest <- function(maf_cancer_sub1, maf_cancer_sub2, sub1, sub2){
+    pt.vs.rt <- mafCompare(m1 = maf_cancer_sub1, m2 = maf_cancer_sub2, m1Name = sub1, m2Name = sub2, minMut = 5)
+    pdf(paste("Forest_", sub1, "_", sub2, ".pdf", sep = ""))
+    forestPlot(mafCompareRes = pt.vs.rt, pVal = 0.1, color = c('royalblue', 'maroon'), geneFontSize = 0.8)
+    dev.off()
+  }
+  
+  PerCoOncoplot <- function(maf_cancer_sub1, maf_cancer_sub2, sub1, sub2, gene_list = gene_list, clinicalFeatures = clinicalFeatures, 
+                            annotationColor = annotationColor){
+    pdf(paste("CoOncoplot_", sub1, "_", sub2, ".pdf", sep = ""), 10, 6)
+    coOncoplot(m1 = maf_cancer_sub1, m2 = maf_cancer_sub2, m1Name = sub1, m2Name = sub2, genes = gene_list, removeNonMutated = TRUE)
+    dev.off()
+  }
+  
+  if(phe_type == 'subtype'){
+    tsb_sub1 <- maf_val@clinical.data$Tumor_Sample_Barcode[which(maf_val@clinical.data$subtype == sub1)]
+    tsb_sub2 <- maf_val@clinical.data$Tumor_Sample_Barcode[which(maf_val@clinical.data$subtype == sub2)]
+    
+    maf_cancer_sub1 <- subsetMaf(maf_val, tsb = tsb_sub1, restrictTo = "all", mafObj = TRUE)
+    maf_cancer_sub2 <- subsetMaf(maf_val, tsb = tsb_sub2, restrictTo = "all", mafObj = TRUE)
+    
+    for(maf_per in c(maf_val, maf_cancer_sub1, maf_cancer_sub2)){
+      P_nam = dim(maf_per@variants.per.sample)[1]
+        PermafSum(maf_per, P_nam = P_nam)
+        Peroncoplot(maf_per, P_nam = P_nam, gene_list = gene_list, clinicalFeatures = clinicalFeatures, annotationColor = annotationColor)
+        Peroncostrip(maf_per, P_nam = P_nam, gene_list = gene_list, clinicalFeatures = clinicalFeatures, annotationColor = annotationColor)
+        PerTiTv(maf_per, P_nam = P_nam)
+        PerVaf(maf_per, P_nam = P_nam, gene_list = gene_list)
+        PerInteraction(maf_per, P_nam = P_nam, gene_list = gene_list)
+        lapply(gene_sur, PerSur, maf_per, P_nam = P_nam, col = col_sur)
+    }
+    
+    PerForest(maf_cancer_sub1, maf_cancer_sub2, sub1, sub2)
+    PerCoOncoplot(maf_cancer_sub1, maf_cancer_sub2, sub1, sub2, gene_list = gene_list, clinicalFeatures = clinicalFeatures, 
+                  annotationColor = annotationColor)
+    
+  } else if (phe_type == 'all'){
+    PermafSum(maf_val, P_nam = "ALL")
+    Peroncoplot(maf_val, P_nam = 'ALL', gene_list = gene_list, clinicalFeatures = clinicalFeatures, annotationColor = annotationColor)
+    Peroncostrip(maf_val, P_nam = 'ALL', gene_list = gene_list, clinicalFeatures = clinicalFeatures, annotationColor = annotationColor)
+    PerTiTv(maf_val, P_nam = 'ALL')
+    PerVaf(maf_val, P_nam = 'ALL', gene_list = gene_list)
+    PerInteraction(maf_val,  P_nam = 'ALL', gene_list = gene_list)
+    lapply(gene_sur, PerSur, maf_per, P_nam = 'ALL', col = col_sur)
+  } else {
+  print('you must set phe_type')
+  }
+}
 
+#MafToolSum(maf_sub, phe_type = 'subtype', sub1 = "1", sub2 = "2", clinicalFeatures = NULL, gene_list = NULL, gene_sur = gene_sur, 
+#           ggcolor = ggcolor, annotationColor = annotationColor, col_sur = c("maroon", "royalblue"))
 
--- common_used_codes/subtype_analysis/sub_multiple_methods.R
+++ common_used_codes/subtype_analysis/sub_multiple_methods.R
@@ -1,95 +1,122 @@
 ###
 ###usage: PerSubMain(rt_miRNA_d, rt_sur_m, method_sub = 'hc', rank_sub = 4, data_type = 'miRNA', nrun = 4, seed = 1262118388.71279,  distance = 'pearson')
+library(scatterplot3d)
+library(plot3D)
+
+source('/Users/stead/Documents/SourceTree/R/common_used_codes/survival_analysis/survival_miner.R')
+source('/Users/stead/Documents/SourceTree/R/common_used_codes/survival_analysis/ggsurv_theme/ggsurv_theme_A.R')
+source('/Users/stead/Documents/SourceTree/R/common_used_codes/cluster_figure/scatt_3d_PCA.R')
+
+#do heatmap
+SubHeat <- function(group_sub, rt_sub_ord, data_type, method_sub, num_k, color_type){
+  
+  print('group_sub and the expression mat must have the same samples names')
+  ha1 = HeatmapAnnotation(group_sub = group_sub, 
+                          col = list(group_sub= structure(names = unique(group_sub), color_type[1:length(unique(group_sub))])))
+  pdf(file = paste(data_type, method_sub, num_k, 'ht.pdf', sep = '_'), 10, 8)
+  ht_list <- Heatmap(rt_sub_ord, name = "center scaled expression",
+                     col = colorRamp2(c(min(rt_sub_ord)/3 , (min(rt_sub_ord) + max(rt_sub_ord))/3, max(rt_sub_ord)/2), c(" blue", "white", "red")),
+                     top_annotation = ha1, top_annotation_height = unit(0.5, "cm"), 
+                     cluster_columns = FALSE, column_dend_reorder = FALSE, 
+                     cluster_rows = TRUE, row_dend_reorder = FALSE, 
+                     show_row_dend = TRUE, show_column_dend = FALSE,
+                     show_row_names = FALSE, show_column_names = FALSE, row_names_side = "right") 
+  draw(ht_list, annotation_legend_side = "right", heatmap_legend_side = "right")
+  dev.off()
+  
+  print('heatmap finished')
+}
+
+###survival analysis
+SubSurv <- function(group_sub, rt_sub_ord, rt_sur_m, data_type, method_sub, num_k, color_type){
+  int_id <- intersect(colnames(rt_sub_ord), rt_sur_m$sample)
+  rt_sur_int <- rt_sur_m[, c('X_OS_IND', 'X_OS')][match(int_id, rt_sur_m$sample, nomatch = 0), ]
+  rt_sub_int <- rt_sub_ord[, match(int_id, colnames(rt_sub_ord), nomatch = 0)]
+  group_int <- group_sub[match(int_id, names(group_sub), nomatch = 0)]
+  
+  rt_sub_sur <- data.frame(cbind(colnames(rt_sub_int), rt_sur_int, group_int))
+  subtye_name <- paste(data_type, method_sub, num_k, sep = '_')
+  colnames(rt_sub_sur) <- c('samples_id', 'OS_Status', 'OS_Time', subtye_name)
+  
+  DrawSurminer(subtye_name, rt_sub_sur, DatType = "LogType", theme_surv_a, s_cutoff = NULL,  
+               palette = color_type[1:length(unique(group_sub))], risk.table = TRUE, fun = NULL)
+  print('survival finished')
+}
+
+SubPCA <- function(group_sub, rt_sub_ord, data_type, method_sub, num_k, color_type){
+  rt_pca <- data.frame(t(rt_sub_ord), stringsAsFactors = FALSE)
+  target_genes <- colnames(rt_pca)
+  nam <-  paste(data_type, method_sub, num_k, sep = '_')
+  sub_color <- as.character(factor(group_sub, labels = color_type[1:length(unique(group_sub))]))
+  
+  rt_sam <- data.frame(cbind(names(group_sub), group_sub, sub_color))
+  colnames(rt_sam) <-  c('samples', 'group', 'color')
+  MakPCA(rt_pca, rt_sam = rt_sam, target_genes, nam = nam, phi = 40, pch = 18, len_a = len_a, len_b = len_b,
+         len_position = 'bottom', width = NULL, height = NULL)
+}
 
 
-PerSubMain <- function(rt_sub, rt_sur_m, method_sub = method_sub, rank_sub = rank_sub, data_type = data_type, nrun = 10, seed = 1262118388.71279, 
+PerSubMain <- function(num_k, rt_sub, rt_sur_m, method_sub = method_sub, rank_sub = rank_sub, data_type = data_type, nrun = 10, seed = 1262118388.71279, 
                        distance = 'pearson'){
-  #
-  color_type <- c("firebrick2", "plum3", "green4", "sandybrown", "gold3", "orange1", "yellow2", "peru")
+  
   mat_sub <- as.matrix(rt_sub)
+  
   if(method_sub %in% c('hc', 'km', 'kmdist', 'pam')){
     res_sub = ConsensusClusterPlus(mat_sub, maxK = rank_sub, reps = 100, pItem = 0.8, pFeature = 1, title = paste(data_type, method_sub, sep = '_'),
                                    clusterAlg = method_sub, distance = distance, seed = seed, plot = "pdf")
     print('subtype finished')
-    for(i in 2:rank_sub){
-      sub_type <- as.data.frame(res_sub[[i]]['consensusClass'])
-      rt_sub_ord <- rt_sub[, match(row.names(sub_type)[order(sub_type$consensusClass)], colnames(rt_sub), nomatch = 0)]
-      sub_type_ord <- sub_type[order(sub_type$consensusClass), ]
-      names(sub_type_ord) <- row.names(sub_type)[order(sub_type$consensusClass)]
-      write.table(sub_type_ord, file = paste(data_type, method_sub, i, 'tab.txt', sep = '_'), row.names = TRUE, col.names = TRUE, sep = '\t')
-      
-      #do heatmap
-      group_sub <- sub_type_ord
-      ha1 = HeatmapAnnotation(group_sub = group_sub, 
-                              col = list(group_sub= structure(names = unique(group_sub), color_type[1:length(unique(group_sub))])))
-      
-      ###show value distribution
-      pdf(file = paste(data_type, method_sub, i, 'ht.pdf', sep = '_'), 10, 8)
-      ht_list <- Heatmap(rt_sub_ord, name = "center scaled expression", col = colorRamp2(c(min(mat_sub)/3 , (min(mat_sub) + max(mat_sub))/3, max(mat_sub)/2), c(" blue", "white", "red")),
-                         top_annotation = ha1, top_annotation_height = unit(0.5, "cm"), 
-                         cluster_columns = FALSE, column_dend_reorder = FALSE, 
-                         cluster_rows = TRUE, row_dend_reorder = FALSE, 
-                         show_row_dend = FALSE, show_column_dend = FALSE,
-                         show_row_names = FALSE, show_column_names = FALSE, row_names_side = "right") 
-      draw(ht_list, annotation_legend_side = "right", heatmap_legend_side = "right")
-      dev.off()
-      print('heatmap finished')
-      
-      ###survival analysis
-      rt_sur_ord <- rt_sur_m[, c('X_OS_IND', 'X_OS')][match(colnames(rt_sub_ord), rt_sur_m$sample, nomatch = 0), ]
-      rt_sub_sur <- data.frame(cbind(colnames(rt_sub_ord), rt_sur_ord, group_sub))
-      colnames(rt_sub_sur) <- c('samples_id', 'status', 'time', 'subtype')
-      pdf(paste(data_type, method_sub, i, 'sur.pdf', sep = '_'), onefile = FALSE)
-      rt_sur_tmp <<- rt_sub_sur
-      fit <- survfit(Surv(time, status) ~ subtype, data = rt_sur_tmp)
-      sur_pt <- ggsurvplot(fit, pval = TRUE)
-      print(sur_pt)
-      dev.off()
-      print('survival finished')
-    }
+    
+    sub_type <- as.data.frame(res_sub[[num_k]]['consensusClass'])
+    rt_sub_ord <- rt_sub[, match(row.names(sub_type)[order(sub_type$consensusClass)], colnames(rt_sub), nomatch = 0)]
+    sub_type_ord <- sub_type[order(sub_type$consensusClass), ]
+    names(sub_type_ord) <- row.names(sub_type)[order(sub_type$consensusClass)]
+    write.table(sub_type_ord, file = paste(data_type, method_sub, num_k, 'tab.txt', sep = '_'), row.names = TRUE, col.names = TRUE, sep = '\t')
+    
+    group_sub <- sub_type_ord  
+    #do heatmap
+    SubHeat(group_sub, rt_sub_ord, data_type, method_sub, num_k, color_type)
+    
+    #do survival
+    SubSurv(group_sub, rt_sub_ord, rt_sur_m, data_type, method_sub, num_k, color_type)
+    
+    #do pca
+    SubPCA(group_sub, rt_sub_ord, data_type, method_sub, num_k, color_type)
+    return(group_sub)
+    
   } else if (method_sub == 'NMF'){
     sub_er <- nmf(mat_sub, 2:rank_sub, nrun = nrun, seed = seed)
     #do a rank survey
+    
     pdf(file = paste(data_type, method_sub, 'nmf_rank_survey.pdf', sep = '_'))
     sub_pl <- plot(sub_er)
     print(sub_pl)
     dev.off()
+    
     pdf(file = paste(data_type, method_sub, 'nmf_rank_ht.pdf', sep = '_'))
     cens_pl <- consensusmap(sub_er)
     print(cens_pl)
     dev.off()
     
-    for(i in 2:rank_sub){
-      res_sub_s <- nmf(mat_sub, i, nrun = nrun, .options = 't')
-      sub_type <- predict(res_sub_s)
-      rt_sub_ord <- rt_sub[, match(names(sub_type)[order(sub_type)], colnames(rt_sub), nomatch = 0)]
-      sub_type_ord <- sub_type[order(sub_type)]
-      write.table(sub_type_ord, file = paste(data_type, method_sub, i, 'tab.txt', sep = '_'), row.names = TRUE, col.names = TRUE, sep = '\t')
+    for(i in 2: num_k){
       
-      #do heatmap
-      group_sub <- sub_type_ord
-      ha1 = HeatmapAnnotation(group_sub = group_sub, 
-                              col = list(group_sub = structure(names = unique(group_sub), color_type[1:length(unique(group_sub))])))
-      pdf(file = paste(data_type, method_sub, i, 'ht.pdf', sep = '_'), 10, 8)
-      ht_list <- Heatmap(rt_sub_ord, name = "center scaled expression", col = colorRamp2(c(min(mat_sub) , (min(mat_sub) + max(mat_sub))/2, max(mat_sub)), c(" blue", "white", "red")),
-                         top_annotation = ha1, top_annotation_height = unit(0.5, "cm"), 
-                         cluster_columns = FALSE, column_dend_reorder = FALSE, 
-                         cluster_rows = TRUE, row_dend_reorder = FALSE, 
-                         show_row_dend = FALSE, show_column_dend = FALSE,
-                         show_row_names = FALSE, show_column_names = FALSE, row_names_side = "right") 
-      draw(ht_list, annotation_legend_side = "right", heatmap_legend_side = "right")
-      dev.off()
-      
-      #perform survival analysis
-      rt_sub_sur = data.frame(cbind(colnames(rt_sub_ord), rt_sur_m[, c('X_OS_IND', 'X_OS')][match(colnames(rt_sub_ord), rt_sur_m$sample, nomatch = 0), ], group_sub))
-      colnames(rt_sub_sur) = c('samples_id', 'status', 'time', 'subtype')
-      rt_sur_tmp <<- rt_sub_sur
-      pdf(paste(data_type, method_sub, i, 'sur.pdf', sep = '_'), onefile = FALSE)
-      fit <- survfit(Surv(time, status) ~ subtype, data = rt_sur_tmp)
-      sur_pt <- ggsurvplot(fit, pval = TRUE)
-      print(sur_pt)
-      dev.off()
+    res_sub_s <- nmf(mat_sub, i, nrun = nrun, .options = 't')
+    sub_type <- predict(res_sub_s)
+    rt_sub_ord <- rt_sub[, match(names(sub_type)[order(sub_type)], colnames(rt_sub), nomatch = 0)]
+    sub_type_ord <- sub_type[order(sub_type)]
+    write.table(sub_type_ord, file = paste(data_type, method_sub, i, 'tab.txt', sep = '_'), row.names = TRUE, col.names = TRUE, sep = '\t')
+
+    group_sub <- sub_type_ord
+    
+    #do heatmap
+    SubHeat(group_sub, rt_sub_ord, data_type, method_sub, i, color_type)
+    
+    #do survival
+    SubSurv(group_sub, rt_sub_ord, rt_sur_m, data_type, method_sub, i, color_type)
+    
+    #do pca
+    SubPCA(group_sub, rt_sub_ord, data_type, method_sub, i, color_type) 
+    
+    return(group_sub)
     }
   }
-}
-
+  }
